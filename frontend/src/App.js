b3fbeb2134be0c6132ca3c00ac598d0ed122c662
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-07-07 12:53:21 +0530

    Connected fronend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Data from "./components/data-list/data-list.component";
-import Pagination from "./components/pagination/pagination.component";
+// import Pagination from "./components/pagination/pagination.component";
+import { Pagination } from "antd";
 
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([{}]);
   const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([{}]);
+  const [total, setTotal] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
-  const [dataPerPage] = useState(35);
+  const [dataPerPage, setDataPerPage] = useState(10);
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       const res = await axios.get("http://localhost:8080/api/v1/data");
       setData(res.data.results);
+      setTotal(res.data.results.length);
       setLoading(false);
     };
     fetchData();
@@ -26,17 +29,37 @@ function App() {
   const indexOfFirstData = indexOfLastData - dataPerPage;
   const currentData = data.slice(indexOfFirstData, indexOfLastData);
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const onShowSizeChange = (current, pageSize) => {
+    setDataPerPage(pageSize);
+    setCurrentPage(1);
+  };
+
+  const itemRender = (current, type, originalElement) => {
+    if (type === "prev") {
+      return <a>Previous</a>;
+    }
+    if (type === "next") {
+      return <a>Next</a>;
+    }
+    return originalElement;
+  };
 
   return (
     <div className="container mt-5">
-      <h1 className="text-primary mb">My New Fetcher App</h1>
+      <h1 className="text-primary mb">The Journal Manager</h1>
       <Data data={currentData} loading={loading} />
-      <Pagination
-        dataPerPage={dataPerPage}
-        totalData={data.length}
-        paginate={paginate}
-      />
+      <div class="pb-5">
+        <Pagination
+          onChange={(pageNumber) => setCurrentPage(pageNumber)}
+          pageSize={dataPerPage}
+          total={total}
+          currentPage={currentPage}
+          showSizeChanger
+          showQuickJumper
+          onShowSizeChange={onShowSizeChange}
+          itemRender={itemRender}
+        />
+      </div>
     </div>
   );
 }
--- frontend/src/components/data-card/data-card.component.jsx
+++ frontend/src/components/data-card/data-card.component.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 const DataCard = ({ item }) => {
   return (
-    <div>
-      <li className="list-group-item">{item.title}</li>
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5 class="card-title">
+          {item.insight}, {item.country}
+        </h5>
+        <h5 class="card-title"></h5>
+        <h6 class="card-text">{item.title}</h6>
+        <h6 class="card-text">Source: {item.source}</h6>
+        <p class="card-title">
+          Sector: {item.sector} &nbsp; &nbsp; &nbsp; Topic: {item.topic}
+        </p>
+
+        <a href={item.url} class="btn btn-primary">
+          Read Now
+        </a>
+      </div>
     </div>
   );
 };
