71a3475ce492d59a7dfe64c4bc8688888dec0f4a
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-07-07 17:37:14 +0530

    Updates

---
+++ precache-manifest.f2bb1517efeede6e1769da7c80cb8776.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "661d4e3ec96459756849",
+    "url": "/static/css/main.eb11d65c.chunk.css"
+  },
+  {
+    "revision": "661d4e3ec96459756849",
+    "url": "/static/js/main.661d4e3e.chunk.js"
+  },
+  {
+    "revision": "588dd365cc8981181848",
+    "url": "/static/js/1.588dd365.chunk.js"
+  },
+  {
+    "revision": "44aff635ce789a2876ad",
+    "url": "/static/css/2.f4ddc19d.chunk.css"
+  },
+  {
+    "revision": "44aff635ce789a2876ad",
+    "url": "/static/js/2.44aff635.chunk.js"
+  },
+  {
+    "revision": "8a05fe733e2104018050",
+    "url": "/static/js/runtime~main.8a05fe73.js"
+  },
+  {
+    "revision": "9d0e191e951703187dc870de4fba4406",
+    "url": "/static/media/Bign.9d0e191e.png"
+  },
+  {
+    "revision": "c9abbe4e5d13d457eb4663e06af032b4",
+    "url": "/index.html"
+  }
+];
